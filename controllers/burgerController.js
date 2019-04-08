a09b503cf4f3fdc8c6421fb8d6e9f6f9c4e9c180
Planet15 <parmar0815@gmail.com>
2019-04-07 22:54:09 -0400

    finished with burgerController. Now figuring out the on click/on submit functions in burgers.

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -29,8 +29,8 @@ router.get("/", function(req, res) {
   
     console.log("condition", condition);
   
-    cat.update({
-      sleepy: req.body.sleepy
+    burger.update({
+      devoured: req.body.sleepy
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
@@ -40,3 +40,18 @@ router.get("/", function(req, res) {
       }
     });
   });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+  module.exports = router;
